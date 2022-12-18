3317b96913e81dbda8c0130fe586b0bb4d01d67a
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2022-12-18 20:17:43 +0530

    unit test cases

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,6 +1,52 @@
-import { render } from '@testing-library/react';
+import { screen, render, getByRole, fireEvent } from '@testing-library/react';
 import Button, { ButtonType } from './Button.component';
 
-test('components/Button', () => {
-  render(<Button type={ButtonType.primary} label='Primary'/>);
+describe('render primary button', () => {
+  const label = 'primary button';
+
+  it('render button', async () => {
+    render(<Button type={ButtonType.primary} label={label} />);
+    const button = await screen.findByRole('button', { name: label });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass('ant-btn');
+    expect(button).toHaveClass('ant-btn-primary');
+  });
+  it('disable button', async () => {
+    render(<Button type={ButtonType.primary} label={label} disabled={true} />);
+    const disabledButton = await screen.findByRole('button', { name: label });
+    expect(disabledButton).toBeDisabled();
+  });
+
+  it('click button', async () => {
+    const onClick = jest.fn();
+    render(<Button type={ButtonType.primary} label={label} onClick={onClick} />);
+    const button = await screen.findByRole('button', { name: label });
+    fireEvent.click(button);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('render link button', () => {
+  const label = 'link button';
+
+  it('render button', async () => {
+    render(<Button type={ButtonType.link} label={label} />);
+    const button = await screen.findByRole('button', { name: label });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass('ant-btn');
+    expect(button).toHaveClass('ant-btn-link');
+  });
+  it('disable button', async () => {
+    render(<Button type={ButtonType.link} label={label} disabled={true} />);
+    const disabledButton = await screen.findByRole('button', { name: label });
+    expect(disabledButton).toBeDisabled();
+  });
+
+  it('click button', async () => {
+    const onClick = jest.fn();
+    render(<Button type={ButtonType.link} label={label} onClick={onClick} />);
+    const button = await screen.findByRole('button', { name: label });
+    fireEvent.click(button);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
 });
